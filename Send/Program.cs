1b699c4ded98354a043151cd2d6b7ff014acf14e
Salih Ozturk <salihozturk@yahoo.com>
2021-05-27 00:12:52 +0200

    Add project files.

---
+++ Send/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connFactory = new ConnectionFactory { HostName = "localhost" , Port  =8081 };
+            using (var connection = connFactory.CreateConnection())
+            
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+                    string message = args[0];
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                        routingKey: "hello",
+                        basicProperties: null,
+                        body: body);
+                    Console.WriteLine($"[x] Sent {message}");
+                    
+
+                }
+                Console.WriteLine("Press enter to exit");
+                Console.WriteLine();
+            }
+        
+    }
+}
